853a949e79cbabc7675164ef7c622c430a9c97d5
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-02-08 08:32:52 -0500

    create the login and admin permissions

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import './app.css';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
 import Topbar from './components/Topbar/Topbar.jsx';
-import Home from './pages/Home/Home.jsx';
-import NewProduct from "./pages/NewProduct/NewProduct";
-import NewUser from "./pages/NewUser/NewUser";
-import Product from "./pages/Product/Product";
-import ProductList from "./pages/ProductList/ProductList";
-import User from "./pages/User/User";
-import UserList from './pages/UserList/UserList.jsx';
 
 function App() {
+
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin;
+
   return (
-    <BrowserRouter>
-      <Topbar />
-      <div className='container'>
-        <Sidebar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/users' element={<UserList />} />
-          <Route path='/user/:userId' element={<User />} />
-          <Route path='/newUser' element={<NewUser />} />
-          <Route path='/products' element={<ProductList />} />
-          <Route path='/product/:productId' element={<Product />} />
-          <Route path='/newproduct' element={<NewProduct />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      {admin &&
+        <>
+          <Topbar />
+          <div className='container'>
+            <Sidebar />
+            <Outlet />
+          </div>
+        </>
+      }
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from "./pages/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Home from './pages/Home/Home';
+import NewProduct from "./pages/NewProduct/NewProduct";
+import NewUser from "./pages/NewUser/NewUser";
+import Product from "./pages/Product/Product";
+import ProductList from "./pages/ProductList/ProductList";
+import User from "./pages/User/User";
+import UserList from './pages/UserList/UserList.jsx';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<Provider store={store}>
+  <PersistGate loading='null' persistor={persistor}>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<Home />} />
+            <Route path='users' element={<UserList />} />
+            <Route path='user/:userId' element={<User />} />
+            <Route path='newUser' element={<NewUser />} />
+            <Route path='products' element={<ProductList />} />
+            <Route path='product/:productId' element={<Product />} />
+            <Route path='newproduct' element={<NewProduct />} />
+          </Route>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </PersistGate>
+</Provider>,
   document.getElementById('root')
 );
 
